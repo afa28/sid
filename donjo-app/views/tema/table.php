003c4d848b8119ceb1db99b3b04fafa5d38056bd
afa28 <andifahruddinakas@gmail.com>
2020-05-05 06:19:53 +0700

    coba aja

--- donjo-app/views/tema/table.php
+++ donjo-app/views/tema/table.php
@@ -7,154 +7,145 @@
 		</ol>
 	</section>
 	<section class="content" id="maincontent">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="box box-info">
-					<div class="box-header with-border">
-						<?= $this->session->flashdata('msg') ?>
-					</div>
-					<div class="box-body">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="box-header with-border">
-									<form method='post' action=<?= site_url("zip/extract") ?> enctype='multipart/form-data'>
-										<div class="row">
-											<div class="col-sm-12">
-												<table class="table table-bordered table-hover">
-													<tbody>
-														<tr>
-															<td style="padding-top:20px;padding-bottom:10px;">
-																<div class="form-group">
-																	<label for="file" class="col-md-2 col-lg-2 control-label">Theme .zip:</label>
-																	<div class="col-sm-12 col-md-8 col-lg-8">
-																		<div class="input-group input-group-sm">
-																			<input type='text' class="form-control" id="file_path" name="userfile">
-																			<input type="file" name='file' class="hidden" id="file" name="userfile" accept="application/sql">
-																			<span class="input-group-btn">
-																				<button type="button" class="btn btn-info btn-flat" id="file_browser"><i class="fa fa-search"></i> Browse</button>
-																			</span>
-																		</div>
-																	</div>
-																	<div class="col-sm-12 col-md-2 col-lg-2">
-																		<button type="submit" name="submit" value="Upload & Extract" class="btn btn-block btn-success btn-sm"><i class="fa fa-spin fa-refresh"></i> Upload</button>
-																	</div>
-																</div>
-															</td>
-														</tr>
-													</tbody>
-												</table>
+		<form method='post' action=<?= site_url("zip/extract") ?> enctype='multipart/form-data'>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="box box-info">
+						<table class="table table-bordered table-hover">
+							<tbody>
+								<tr>
+									<td style="padding-top:20px;padding-bottom:10px;">
+										<div class="form-group">
+											<label for="file" class="col-md-2 col-lg-2 control-label">Tema .zip:</label>
+											<div class="col-sm-12 col-md-8 col-lg-8">
+												<div class="input-group input-group-sm">
+													<input type="text" class="form-control" id="file_path2">
+													<input type="file" class="hidden" id="file2" name="tema">
+													<span class="input-group-btn">
+														<button type="button" class="btn btn-info btn-flat" id="file_browser"><i class="fa fa-search"></i> Browse</button>
+													</span>
+												</div>
+											</div>
+											<div class="col-sm-12 col-md-2 col-lg-2">
+												<button type="submit" name="submit" value="Upload & Extract" class="btn btn-block btn-success btn-sm"><i class="fa fa-spin fa-refresh"></i> Upload</button>
 											</div>
 										</div>
-									</form>
+										<br><br>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</form>
+		<div class="row"><!-- Untuk Tema Yg Active -->
+			<div class="col-md-4 col-xs-12">
+				<div class="box box-danger"> <!-- <div class="box box-danger">  -->
+					<div class="box-header with-border">
+						<center><h3 class="box-title text-center"><?= $this->theme ?></h3></center>
+					</div>
+					<div class="box-body">
+						<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+								<div class="item active">
+									<img class="img-responsive" src="<?= base_url().$this->theme_folder.'/'.$this->theme.'/thumbnail/1.png'; ?>" alt="First slide">
 								</div>
-								<div class="box-body">
-
+								<div class="item">
+									<img class="img-responsive" src="<?= base_url().$this->theme_folder.'/'.$this->theme.'/thumbnail/2.png'; ?>" alt="First slide">
 								</div>
+								<div class="item">
+									<img class="img-responsive" src="<?= base_url().$this->theme_folder.'/'.$this->theme.'/thumbnail/3.png'; ?>" alt="First slide">
+								</div>
+								<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+									<span class="fa fa-angle-left"></span>
+								</a>
+								<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+									<span class="fa fa-angle-right"></span>
+								</a>
+							</div>
+						</div>
+						<div class="text-center">
+							<div class="btn-group">
+								<button type="button" class="btn btn-success btn-sm disabled">
+									<i class="fa fa-star"></i>
+								</button>
+								<!--<a href="" class="btn btn-success btn-sm disabled"  title="Active"><i class="fa fa-star"></i>Active</a>-->
+								<a href="<?= site_url("tema/edit/" . $nama . '/' . $kat) ?>" class="btn btn-primary btn-sm"  title="Edit"><i class="fa fa-pencil"></i></a>
+								<button type="button" class="btn btn-danger btn-sm disabled">
+									<i class="fa fa-trash-o"></i>
+								</button>
+								<a href="<?= site_url("tema/backup/" . $nama . '/' . $kat) ?>" class="btn bg-navy btn-sm"  title="Backup"><i class="fa fa-download"></i></a>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="row">
-			<!--Tema Yang Aktif-->
 			<?php
-			$nama_tema = explode("/",  $tema_aktif );
+			$no = 1;
+			foreach ($list_tema as $tema) :
+				$tema = explode('/', $tema);
 
-			if ($nama_tema[0] == 'desa') {
-				$nama = $nama_tema[1];
-					$kat = 0; //desa/themes/
-				} else {
-					$nama = $nama_tema[0];
-					$kat = 1; //themes
-				}
-				?>
-				<div class="col-md-4 col-xs-12 col-center">
-					<div class="box box-info">
-						<div class="box-body box-profile">
-							<img class="img-responsive" src="<?= base_url() ?>
-							<?= ($kat == 0) ? 'desa/themes/' : 'themes/' ?>
-							<?= $nama . '/thumbnail.png' ?>" alt=" <?= $nama ?>">
-						</div>
-						<div class="row">
-							<div class="col-sm-12">
-								<div class="form-group">
-									<label for="file" class="col-md-4 col-lg-4 control-label" align="left"><b><?= ucwords($nama); ?></b></label>
-									<div class="col-sm-12 col-md-8 col-lg-8">
-										<a href="#" class="btn btn-block btn-primary btn-sm" disabled=true><i class="fa fa-spin fa-refresh"></i> Sedang Digunakan</a>
+				if($tema[0] != 'desa'):
+					$nama = $tema[0];
+					$lokasi = 'themes/'.$nama;
+				else:
+					$nama = $tema[1];
+					$lokasi = $tema[0].'/themes/'.$nama;
+				endif;
+
+				if($nama != $this->theme):
+					$no++; ?>
+					<div class="col-md-4 col-xs-12">
+						<div class="box box-info"> <!-- <div class="box box-danger">  -->
+							<div class="box-header with-border">
+								<center><h3 class="box-title"><?= ucwords($nama) ?></h3></center>
+							</div>
+							<div class="box-body">
+								<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+									<div class="carousel-inner">
+										<div class="item active">
+											<img class="img-responsive" src="<?= base_url().$lokasi.'/thumbnail/1.png'; ?>" alt="<?= $nama ?>-1">
+										</div>
+										<div class="item">
+											<img class="img-responsive" src="<?= base_url().$lokasi.'/thumbnail/2.png'; ?>" alt="<?= $nama ?>-2">
+										</div>
+										<div class="item">
+											<img class="img-responsive" src="<?= base_url().$lokasi.'/thumbnail/3.png'; ?>" alt="<?= $nama ?>-3">
+										</div>
+										<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+											<span class="fa fa-angle-left"></span>
+										</a>
+										<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+											<span class="fa fa-angle-right"></span>
+										</a>
 									</div>
 								</div>
-								<br>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!--Tema Standar dan Tidak Aktif-->
-				<?php if($tema_aktif !='hadakewa'){ ?>
-					<div class="col-md-4 col-xs-12 col-center">
-						<div class="box box-info">
-							<div class="box-body box-profile">
-								<img class="img-responsive" src="<?= base_url().'themes/hadakewa/thumbnail.png' ?>" alt="hadakewa" >
-							</div>
-							<div class="row">
-								<div class="col-sm-12">
-									<div class="form-group">
-										<label for="file" class="col-md-4 col-lg-4 control-label" align="left"><b>hadakewa</b></label>
-										<div class="col-sm-12 col-md-8 col-lg-8">
-											<a href="<?= site_url("tema/ganti_tema/hadakewa") ?>" class="btn btn-block btn-primary btn-sm"><i class="fa fa-lock"></i> Standar</a>
+								<div class="text-center">
+									<div class="btn-group">
+										<a href="" class="btn btn-success btn-sm"  title="Active"><i class="fa fa-star"></i></a>
+										<a href="<?= site_url("tema/edit/" . $nama) ?>" class="btn btn-primary btn-sm"  title="Edit"><i class="fa fa-pencil"></i></a>
+										<?php if($lokasi == 'themes'):?>
+											<button type="button" class="btn btn-danger btn-sm disabled">
+												<i class="fa fa-trash-o"></i>
+											</button>
+											<?php else : ?>
+												<a href="#" data-href="<?= site_url("tema/delete/" . $nama) ?>" class="btn btn-danger btn-sm"  title="Delete" data-toggle="modal" data-target="#confirm-delete"><i class="fa fa-trash-o"></i></a>
+											<?php endif;?>
+											<a href="<?= site_url("tema/backup/" . $nama) ?>" class="btn bg-navy btn-sm"  title="Backup"><i class="fa fa-download"></i></a>
 										</div>
 									</div>
-									<br>
 								</div>
 							</div>
-						</div>
-					</div>
-				<?php } ?>
-
-				<!--Tema Lainnnya Yang Ada Di Direktori-->
-				<?php foreach ($list_tema as $tema => $data) {
-					$nama_tema = explode("/",  $data);
-
-					if ($nama_tema[0] == 'desa') {
-						$nama = $nama_tema[1];
-					$kat = 0; //desa/themes/
-				} else {
-					$nama = $nama_tema[0];
-					$kat = 1; //themes
-				}
-				?>
-				<?php if($tema_aktif != $data AND $data !='hadakewa'){ ?>
-					<div class="col-md-4 col-xs-12 col-center">
-						<div class="box box-info">
-							<div class="box-body box-profile">
-								<img class="img-responsive" src="<?= base_url() ?>
-								<?= ($kat == 0) ? 'desa/themes/' : 'themes/' ?>
-								<?= $nama . '/thumbnail.png' ?>" alt=" <?= $nama ?>" >
+							<?php if($no % 3 == 0):?>
 							</div>
 							<div class="row">
-								<div class="col-sm-12">
-									<div class="form-group">
-										<label for="file" class="col-md-4 col-lg-4 control-label" align="left"><b><?= ucwords($nama); ?></b></label>
-										<?php if ($data == 'hadakewa') { ?>
-											<div class="col-sm-12 col-md-8 col-lg-8">
-												<a href="<?= site_url("tema/ganti_tema/" . $data) ?>" class="btn btn-block btn-primary btn-sm"><i class="fa fa-lock"></i> Standar</a>
-											</div>
-										<?php } else { ?>
-											<div class="col-sm-12 col-md-3 col-lg-4">
-												<a href="<?= site_url("tema/ganti_tema/" . $data) ?>" class="btn btn-block btn-success btn-sm"><i class="fa fa-lock"></i> Aktifkan</a>
-											</div>
-											<div class="col-sm-12 col-md-3 col-lg-4">
-												<a href="<?= site_url("tema/delete/" . $nama . '/' . $kat) ?>" class="btn btn-block btn-danger btn-sm"><i class="fa fa-trash-o"></i> Hapus</a>
-											</div>
-										<?php }  ?>
-									</div>
-									<br>
-								</div>
-							</div>
+							<?php endif;?>
 						</div>
-					</div>
-				<?php }  ?>
-			<?php } ?>
+					<?php endif;
+				endforeach; ?>
+			</div>
 		</div>
 	</section>
 </div>
+<?php $this->load->view('global/confirm_delete');?>
