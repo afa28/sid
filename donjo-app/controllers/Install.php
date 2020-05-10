b4583e7aeb7d826da0784c0fffa76b215f890b00
afa28 <andifahruddinakas@gmail.com>
2020-05-11 00:59:15 +0700

    ap[daipf

--- donjo-app/controllers/Install.php
+++ donjo-app/controllers/Install.php
@@ -7,7 +7,7 @@ class Install extends CI_Controller
 	public function index()
 	{
 		// Set preference
-		$folder_upload	=	FCPATH.'assets/themes/';
+		$temp_folder	=	FCPATH.'assets/themes/';
 		$folder_extract	=	FCPATH.'desa/themes/';
 
 		if (!file_exists($folder_extract))
@@ -15,43 +15,36 @@ class Install extends CI_Controller
 			mkdir($folder_extract, 0, true);
 		}
 
-		$config['upload_path'] =  $folder_upload;
-		$config['allowed_types'] = 'zip';
-		$config['max_size'] = '5120'; // max_size in kb (5 MB)
-		$config['file_name'] = $_FILES['file']['name'];
+		$config['upload_path']		=	$temp_folder;
+		$config['allowed_types']	=	'zip';
+		$config['max_size']				=	'5120'; // max_size in kb (5 MB)
+		$config['file_name']			=	$_FILES['file']['name'];
 
-		// Load upload library
 		$this->load->library('upload',$config);
 
-		// File upload
 		if($this->upload->do_upload('file') AND !empty($_FILES['file']['name']))
 		{
-			// Get data about the file
 			$uploadData = $this->upload->data();
 			$filename = $uploadData['file_name'];
-			$link = $folder_upload.$filename;
+			$link = $temp_folder.$filename;
 
-			## Extract the zip file ---- start
 			$zip = new ZipArchive;
-			$res = $zip->open($link);
-			if ($res === TRUE)
+
+			if ($zip->open($link) === TRUE)
 			{
 
-				// Extract file
 				$zip->extractTo($folder_extract);
 				$zip->close();
 
-				$this->session->set_flashdata('msg','Upload & Extract successfully.');
-			}
-			else
-			{
-				$this->session->set_flashdata('msg','Failed to extract.');
+				$msg = 'Tema berhasil di install';
 			}
 		}
 		else
 		{
-			$this->session->set_flashdata('msg','Failed to upload');
+			$msg = $this->upload->display_errors();
 		}
+
+		$this->session->set_flashdata('msg', $msg);
 
 		redirect('tema');
 	}
