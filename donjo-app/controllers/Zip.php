989c340f7c8989144fdeded9600b2e7cc2ff2a9e
afa28 <andifahruddinakas@gmail.com>
2020-05-10 14:18:03 +0700

    sgs

---
+++ donjo-app/controllers/Zip.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class zip extends CI_Controller
+{
+
+	// Upload and Extract zip file
+
+
+}
--- donjo-app/models/Theme_model.php
+++ donjo-app/models/Theme_model.php
@@ -27,10 +27,53 @@
 	public function active()
 	{
 		$active = $this->db->select('value')
-			->where('key', 'web_theme')
-			->get('setting_aplikasi')->row()->value;
+		->where('key', 'web_theme')
+		->get('setting_aplikasi')->row()->value;
 
 		return $active;
 	}
-}
-?>
+
+	public function extract(){
+		if(!empty($_FILES['file']['name'])){
+				// Set preference
+			$folder = 'desa/themes/';
+
+			if (!file_exists($folder))
+			{
+				$folder = 'themes/';
+			}
+			$config['upload_path'] =  $folder;
+			$config['allowed_types'] = 'zip';
+				$config['max_size'] = '5120'; // max_size in kb (5 MB)
+				$config['file_name'] = $_FILES['file']['name'];
+
+								// Load upload library
+				$this->load->library('upload',$config);
+
+								// File upload
+				if($this->upload->do_upload('file')){
+										// Get data about the file
+					$uploadData = $this->upload->data();
+					$filename = $uploadData['file_name'];
+					$link = $folder.$filename;
+
+										## Extract the zip file ---- start
+					$zip = new ZipArchive;
+
+					if ($zip->open($link) === TRUE)
+					{
+
+												// Extract file
+						$zip->extractTo($folder);
+						$zip->close();
+
+						$this->session->set_flashdata('msg','Upload & Extract successfully.');
+				}else{
+					$this->session->set_flashdata('msg','Failed to upload');
+				}
+			}else{
+				$this->session->set_flashdata('msg','Failed to upload');
+			}
+		}
+	}
+	?>
